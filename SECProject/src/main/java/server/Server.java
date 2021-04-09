ac12172a0c9794a3774281eb0409c0f5d6aabee6
Jan Pesek <pesekja9@fit.cvut.cz>
2021-04-09 22:01:02 +0100

    Backbone of server implementation

---
+++ SECProject/src/main/java/server/Server.java
@@ -0,0 +1,39 @@
+package server;
+
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class Server {
+
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+
+        init();
+    }
+
+    private void init() {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                io.grpc.Server server = ServerBuilder.forPort(port).addService(new ServerImp()).build();
+                try {
+                    server.start();
+                    System.out.println("Server started at " + server.getPort());
+
+                    server.awaitTermination();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        new Thread(r).start();
+        System.out.println("Server thread running");
+
+    }
+
+}
--- SECProject/src/main/java/server/ServerImp.java
+++ SECProject/src/main/java/server/ServerImp.java
@@ -1,10 +1,53 @@
 package server;
 
+import com.server.grpc.ServerService;
+import com.server.grpc.ServerService.subLocRepReply;
+import com.server.grpc.ServerService.subLocRepReq;
+
 import com.server.grpc.serverServiceGrpc.serverServiceImplBase;
+import io.grpc.stub.StreamObserver;
 
 
 public class ServerImp extends serverServiceImplBase {
 
-	
+    private enum replyCode {OK, WRONG_EPOCH, NOK}
 
+    @Override
+    public void submitLocationReport(subLocRepReq request, StreamObserver<subLocRepReply> responseObserver) {
+        System.out.println("[Server] Report submit request from " + request.getUserID() +
+                           " at epoch " + request.getEpoch());
+
+        ServerService.subLocRepReply.Builder response = reportHandler(request);
+        responseObserver.onNext(response.build());
+
+        responseObserver.onCompleted();
+
+    }
+
+    private subLocRepReply.Builder reportHandler(subLocRepReq request) {
+        subLocRepReply.Builder response = subLocRepReply.newBuilder();
+
+        if (!checkEpoch(request.getEpoch())) {
+            response.setReplycode(replyCode.WRONG_EPOCH.ordinal());
+            response.setReplymessage("Provided report is not valid for current epoch");
+        }
+
+        if (!submitReport()) {
+            response.setReplycode(replyCode.NOK.ordinal());
+            response.setReplymessage("Your report was not submitted");
+        }
+
+        response.setReplycode(replyCode.OK.ordinal());
+        response.setReplymessage("Your report was submitted successfully");
+
+        return response;
+    }
+
+    private boolean submitReport() {
+        return true;
+    }
+
+    private boolean checkEpoch(String epoch) {
+        return true;
+    }
 }
