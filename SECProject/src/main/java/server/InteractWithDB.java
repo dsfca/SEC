b94fb699a8f3a9bb4297ed1174a63c09ac9f69a5
dsfca <dsfca@iscte-iul.pt>
2021-04-15 16:00:07 +0100

    Database functions complete

--- SECProject/src/main/java/server/InteractWithDB.java
+++ SECProject/src/main/java/server/InteractWithDB.java
@@ -19,7 +19,9 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-
+/**WATCH NOTIFICATIONS BELLOW IN: 
+							- getLocationsGivenEpoch()							
+*/
 
 public class InteractWithDB {
 	
@@ -54,7 +56,7 @@ public class InteractWithDB {
 	}
 	
 	
-	
+	//ESTABLISH CONNECTION TO DATABASE
 	public void connectToMongo() {
 		mongo_client = new MongoClient(new MongoClientURI(getHost()));
 		mongo_db = mongo_client.getDatabase(getDatabase());
@@ -63,23 +65,9 @@ public class InteractWithDB {
 		//System.out.println(mongo_db.getName());
 	}
 
-	/**
-	 * Check username in database
-	 * @param username
-	 * @return
-	 */
-	/*public boolean existsUsername(String username) {
-		MongoCursor<Document> cursor = mongo_collection_validated.find().iterator();
-
-		while (cursor.hasNext()) {
-			if(cursor.next().get("username").equals(username)) {
-				return true;
-			}
-		}
-		return false;
-	}*/
 	
-	//ADDS A REPORT TO COLLECTION "ALL_LOCATIONS"
+	
+	//ADDS PROOF TO DATABSE - COLLECTION "ALL_LOCATIONS"
 	public void addReportToDatabase(int user1, int user2, Point2D pos1, Point2D pos2, int epoch, boolean near) {
 		Document document = new Document();
 		document.append("user1", user1);
@@ -91,32 +79,59 @@ public class InteractWithDB {
 		mongo_collection_all.insertOne(document);
 	}
 	
+	
+	//INSERTS LOCATION TO COLLECTION "VALIDATED"
+	public void addLocationToValidated(int user, Point2D pos, int epoch) {
+		Document document = new Document();
+		document.append("user", user);
+		document.append("location", pos.toString());
+		document.append("epoch", epoch);
+		mongo_collection_validated.insertOne(document);
+	}
+	
+		
+	//IF RETURN "NULL", PLACE EXCEPTION (PRINT TO SCREEN) IN CLASS SERVER THAT THE USER DOESNT HAVE A LOCATION PROOF FOR THAT EPOCH
 	/**
-	 * Get location document given a specific epoch
-	 * @param epoch
+	 * Get location document given a specific user and epoch (USER->SERVER) - obtainLocationReport()
+	 * @param location
 	 * @return
 	 */
-	public Point2D getLocationsGivenEpoch(int user, int epoch) {
+	public Point2D getLocationGivenEpoch(int user, int epoch) {
 		MongoCursor<Document> cursor = mongo_collection_validated.find().iterator();
 		Point2D location = null;
 		
 		while (cursor.hasNext()) {
 			Document current  = cursor.next();
 			if(current.get("epoch").equals(epoch) && current.get("user").equals(user)) {
-				System.out.println(current.toString());
 				String location_string = current.get("location").toString();
-				location = new Point2D(Integer.valueOf(location_string.substring(1, 2)), Integer.valueOf(location_string.substring(3, 4)));
+				location = new Point2D(Integer.valueOf(location_string.substring(1, 2)), Integer.valueOf(location_string.substring(4, 5)));
 			}
 		}
 		return location;
 	}
 	
-	//ERASES COLLECTION "VALIDATED" CONTENT
+	
+	//(HA->SERVER) - obtainUsersAtLocation()
+	public ArrayList<String> getUsersGivenPosAndEpoch(Point2D pos, int epoch) {
+		MongoCursor<Document> cursor = mongo_collection_validated.find().iterator();
+		ArrayList <String> users = new ArrayList <String> ();
+		
+		while (cursor.hasNext()) {
+			Document current  = cursor.next();
+			if(current.get("location").equals(pos.toString()) && current.get("epoch").equals(epoch)) {
+				//System.out.println(current.get("user").toString());
+				users.add(current.get("user").toString());
+			}
+		}
+		return users;
+	}
+	
+	//CLEAN COLLECTION "VALIDATED" CONTENT
 	public void cleanValidatedCollection() {
 		mongo_collection_validated.drop();
 	}
 	
-	//ERASES COLLECTION "ALL_LOCATIONS" CONTENT
+	//CLEAN COLLECTION "ALL_LOCATIONS" CONTENT
 		public void cleanAllLocationsCollection() {
 			mongo_collection_all.drop();
 		}
@@ -126,8 +141,14 @@ public class InteractWithDB {
 		InteractWithDB it = new InteractWithDB("variables.ini");
 		it.cleanValidatedCollection();
 		it.cleanAllLocationsCollection();
-		//it.addReportToDatabase("user1", 0, 0, 0);
-		//ArrayList <String> locations = it.getLocationsGivenEpoch(0);
+		//LOCATIONS
+		it.addLocationToValidated(1, new Point2D(0,0), 1);
+		it.addLocationToValidated(4, new Point2D(0,0), 1);
+		it.addLocationToValidated(2, new Point2D(0,1), 1);
+		
+		//Point2D location = it.getLocationGivenEpoch(1, 2);
+		//System.out.println(location);
+		ArrayList <String> users = it.getUsersGivenPosAndEpoch(new Point2D(0,0), 1);
 	}
 
 }
